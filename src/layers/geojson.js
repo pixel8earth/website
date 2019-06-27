9b19a0afc8d86dae5a0e287d88804713cdd49708
Chris Helm <christopher.helm@gmail.com>
2019-06-27 08:31:35 -0600

    cooler dots

--- src/layers/geojson.js
+++ src/layers/geojson.js
@@ -4,6 +4,23 @@ import SphericalMercator from 'sphericalmercator'
 const basePlaneDimension = 65024;
 const mercator = new SphericalMercator({size: basePlaneDimension});
 
+function generateSprite() {
+  const canvas = document.createElement('canvas');
+  canvas.width = 40;
+  canvas.height = 40;
+  const ctx = canvas.getContext('2d');
+  const gradient = ctx.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+  gradient.addColorStop(0, 'rgba(255,255,255,1)');
+  gradient.addColorStop(0.2, 'rgba(0,255,255,1)');
+  gradient.addColorStop(0.4, 'rgba(0,0,64,1)');
+  gradient.addColorStop(1, 'rgba(0,0,0,1)');
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  const texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+  return texture;
+}
+
 class GeoJSON {
   type = 'GeoJSON'
   loaded = false
@@ -16,12 +33,20 @@ class GeoJSON {
 
   update = (tiles, scene, offsets, render) => {
     if (!this.loaded) {
+      const mat = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 4.0,
+        transparent: true,
+        blending: THREE.AdditiveBlending,
+        map: generateSprite()
+      })
+  
       this.fetchData(this.url, offsets)
         .then(vertices => {
           this.loaded = true
           const geom = new THREE.BufferGeometry()
           geom.addAttribute( 'position', vertices )
-          const points = new THREE.Points( geom, new THREE.PointsMaterial( { color: 0x00ff55, size: 2.0 } ) )
+          const points = new THREE.Points( geom, mat) //new THREE.PointsMaterial( { color: 0x00ff55, size: 2.0 } ) )
           scene.add(points)
           render()
         })
