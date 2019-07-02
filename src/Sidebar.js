308dce6f55167b42dc3d2f773033c8f6a1ad7739
Winnie Palangpour <winnie.mabee@gmail.com>
2019-07-02 18:40:54 -0500

    P8 icon toggling and a start to styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import icon from './images/icon.png';
 
 class Sidebar extends React.Component {
   constructor() {
     super();
+    this.mounted = false;
+
     this.state = {
-      showing: [],
-      groups: []
+      showing: null,
+      groups: [],
+      expanded: false
     };
   }
 
   componentDidMount() {
-    this.setState({ showing: this.props.groups.map(g => g.name), groups: this.props.groups });
+    this.mounted = true;
+    this.setState({ groups: this.props.groups });
+  }
+
+  componentDidUpdate() {
+    if (this.mounted && !this.state.showing) {
+      this.setState({
+        showing: this.props.groups.map(g => g.name)
+      });
+    }
   }
 
   toggleGroup = (group) => () => {
     let showing = [...this.state.showing];
-    const index = this.state.showing.indexOf(group.name);
+    const index = showing.indexOf(group.name);
     if (index < 0) {
       showing.push(group.name);
       this.props.add(group);
@@ -27,28 +40,76 @@ class Sidebar extends React.Component {
     this.setState({ showing });
   };
 
+  toggleExpansion = () => {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
   render() {
-    const {} = this.state;
+    const { groups, showing, expanded } = this.state;
     return (
-      <div id="sidebar" style={styles.main}>
-        { this.state.groups.map( (g, i) => (
-          <div key={i} onClick={ this.toggleGroup(g) } style={styles.group}>
-            {g.name}
+      !expanded ?
+        <div style={styles.collapsed} onClick={this.toggleExpansion}>
+          <img src={icon} style={styles.pixel8IconSmall} />
+        </div>
+        :
+        <div id="sidebar" style={styles.main}>
+          <div style={styles.imgWrap} onClick={this.toggleExpansion}>
+            <img src={icon} style={styles.pixel8Icon} />
           </div>
-        ))}
-      </div>
+          { groups.map( (g, i) => {
+            const shown = ~(showing || []).indexOf(g.name);
+            return (
+              <React.Fragment key={i}>
+                {i === 0 && <hr style={styles.hr} />}
+                <div onClick={this.toggleGroup(g)} style={shown ? styles.groupShown : styles.group}>
+                  {g.name}
+                </div>
+                <hr style={styles.hr} />
+              </React.Fragment>
+            );
+          })}
+        </div>
     );
   }
 }
 
 const styles = {
+  collapsed: {
+    backgroundColor: '#000',
+    width: '60px',
+    height: '60px',
+    position: 'absolute',
+    top: 0,
+    left: 0
+  },
   main: {
-    backgroundColor: 'blue',
+    backgroundColor: '#304D6D',
     color: '#fff'
   },
   group: {
     padding: '10px',
     cursor: 'pointer'
+  },
+  groupShown: {
+    padding: '10px',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    backgroundColor: '#263f59',
+  },
+  imgWrap: {
+    textAlign: 'center',
+    marginTop: '2px'
+  },
+  pixel8Icon: {
+    width: '75px',
+    padding: '5px'
+  },
+  pixel8IconSmall: {
+    width: '50px',
+    padding: '5px'
+  },
+  hr: {
+    margin: 0
   }
 };
 
