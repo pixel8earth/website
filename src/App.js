58d4dcc0ab3616a2d245316fec25492f7dc118f4
Winnie Palangpour <winnie.mabee@gmail.com>
2019-07-08 16:54:14 -0500

    react router added. with /austin and /boulder routes rendering different versions of the map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, {Component} from 'react';
-import ThreeMap from './lib/threemap';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Map from './components/Map';
+import Home from './components/Home';
 import Layers from './layers'
 import './App.css';
-
-/**
-  TODO List
-
-  1. For hosting purposes we probably want to have this App.js render the Pixel8 home page but support loading the visualizations via a route so we need some sort of routing
-     ie: pixel8.earth/austin and pixel8.earth/boulder
-  2. The point tiles have issues with tiles loading out of sync. Like if we're in the middle of fetching a chunk of tiles and the user moves, sometimes tiles that are not fetched yet will show up
-      that are supposed to be hidden, or at least are not in the current view.
-      To solve this i think we need to have some sort of state on the point tile component that either (1) prevent tiles from rendering that are invalid or (2) the promises on the fetches need to be cancelled
-
-  3. Render meshes
-  4. aerial imagery
-  5. source all data providers
-
-  6. animate ground level things... move along a path etc
-
-*/
 
 const points = new Layers.PointTiles('lidar', 'https://pixel8austin.storage.googleapis.com/lidar/tiles/{z}/{x}/{y}.csv.gz')
 //const points = new Layers.PointTiles('lidar', 'http://localhost/~chelm/hdiz/austin/mesh_src/{z}/{x}/{y}.csv.gz')
@@ -29,8 +15,16 @@ const images = new Layers.ImageTiles('basemap', 'https://{s}.basemaps.cartocdn.c
 //const mesh = new Layers.PlyTiles('ground-mesh', 'https://pixel8austin.storage.googleapis.com/mesh/ground/{z}/{x}/{y}.ply.gz')
 //const pc = new Layers.PointCloud('cloud', 'http://localhost/~chelm/hdiz/austin/collects/1561993727406/geo-model.csv', {proj: 'EPSG:32614'})
 
-export default class App extends Component {
-  render() {
-    return (<ThreeMap center={[-97.739677,30.257936]} cam_zoom={1} layers={[images]}/>);
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/austin" component={() => <Map center={[-97.739677,30.257936]} layers={[ images, points ]} />} />
+        <Route path="/boulder" component={() => <Map center={[-105.2705, 40.0150]} layers={[ images ]} />} />
+      </div>
+    </Router>
+  );
 }
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/pixel8.jpg" alt="Future home of Pixel8.earth" width={400}/>
+        Future home of Pixel8.earth
+      </header>
+    </div>
+  );
+}
+
+export default Home;
