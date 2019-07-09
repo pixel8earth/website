1f83b0e9d4adc5c54277b2a22e5d1d7fe4fc7f87
Chris Helm <christopher.helm@gmail.com>
2019-07-09 13:03:09 -0600

    adding shader file

---
+++ src/layers/shaders.js
@@ -0,0 +1,26 @@
+const vert = `
+  attribute vec3 ca;
+  uniform float size;
+  varying vec3 vUv;
+  varying vec3 vColor;
+  void main()
+  {
+      vColor = ca;
+      vUv = position;
+      vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+      gl_PointSize = min(max(size * vUv.z, 0.25), 1.25);
+      gl_Position = projectionMatrix * viewMatrix * worldPosition;
+  }
+`
+
+const frag = `
+  varying vec3 vColor;
+  precision mediump float;
+  void main()
+  {
+      gl_FragColor = vec4(vColor, 1.0);
+      gl_FragColor.a = 1.0;
+  }
+`
+
+module.exports = { vert, frag }
