9e92185d6bb04144fe88e4d87327144489e82e03
Chris Helm <christopher.helm@gmail.com>
2019-07-12 09:01:34 -0600

    comments in the pointcloud layer

--- src/layers/pointcloud.js
+++ src/layers/pointcloud.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three'
-import pako from 'pako'
 import { llPixel } from '../lib/utils'
 import proj4 from 'proj4'
 import Base from './base'
@@ -37,7 +36,6 @@ class PointCloud extends Base {
         .then(geom => {
           this.loaded = true
           const points = new THREE.Points( geom, mat)
-          points.position.y = 0
           this.group.add(points)
           render()
         })
@@ -57,9 +55,6 @@ class PointCloud extends Base {
           if (!res.ok) {
             return reject('not found')
           }
-          if (ext === 'gz') {
-            return pako.inflate(await res.arrayBuffer(), {to: 'string'})
-          }
           return res.text()
         })
         .then(raw => {
@@ -69,16 +64,23 @@ class PointCloud extends Base {
             if (i >= 10) {
               const p = this.splitLine(ext, line)
               if (!isNaN(p[0])) {
+                // NOTE: corrects for order of X/Y/Z in source data
                 const xyz = ext === 'ply' ? [p[2], p[0], p[1]] : [p[0], p[1], p[2]]
+                // go to LatLng
                 const ll = proj4(this.proj, 'EPSG:4326').forward([xyz[0], xyz[1]])
+                // go to pixels
                 let px = llPixel(ll, 0, this.size)
+                // go to world coords in three.js
                 const pt = {x: px[0] - this.size / 2, y: px[1] - this.size / 2, z: xyz[2]}
-                const color = new THREE.Color();
-                color.setRGB(p[3] / 255.0, p[4] / 255.0, p[5] / 255.0)
+                // scale the z coord
                 const zMin = this.options.scales[0] || 130;
                 const zMax = this.options.scales[1] || 350;
                 const scaledZ = ((pt.z - zMin) / (zMax - zMin)) * (this.options.scales[2] || 0.5) + zMin;
+                // apply offsets to x/y
                 points.vertices.push(new THREE.Vector3(pt.x - offsets.x, -pt.y + offsets.y, scaledZ));
+
+                const color = new THREE.Color();
+                color.setRGB(p[3] / 255.0, p[4] / 255.0, p[5] / 255.0)
                 points.colors.push(color);
               }
             }
